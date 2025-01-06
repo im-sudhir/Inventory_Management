c26dbe89a85a96a0950ff634bfd82556b6ba1b08
im-sudhir <ssudhir794@gmail.com>
2025-01-06 20:59:34 +0530

    added serializers, views and urls files for app

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ItemListCreateView
+
+
+urlpatterns=[
+    path('items', ItemListCreateView.as_view(), name="items"),
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView
+from .serializers import InventoryItemSerializer
+from .models import InventoryItem
 # Create your views here.
+
+class ItemListCreateView(ListCreateAPIView):
+    serializer_class=InventoryItemSerializer
+    queryset=InventoryItem.objects.all()
