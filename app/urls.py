6486622fe518ff7c7439b2dcbf3b8bf60a112a75
im-sudhir <ssudhir794@gmail.com>
2025-01-06 21:38:37 +0530

    created detail view for items

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ItemListCreateView
+from .views import ItemListCreateView, ItemDetailView
 
 
 urlpatterns=[
     path('items', ItemListCreateView.as_view(), name="items"),
+    path('items/<int:pk>', ItemDetailView.as_view(), name='item-detailview'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import InventoryItemSerializer
 from .models import InventoryItem
 # Create your views here.
 
 class ItemListCreateView(ListCreateAPIView):
+    serializer_class=InventoryItemSerializer
+    queryset=InventoryItem.objects.all()
+
+class ItemDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=InventoryItemSerializer
     queryset=InventoryItem.objects.all()
