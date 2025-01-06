efc6b5b50a4c5b5bfc7305b5691a12efe8c7157c
im-sudhir <ssudhir794@gmail.com>
2025-01-06 07:30:37 +0530

    added stock status in model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class InventoryItem(models.Model):
     name=models.CharField(max_length=50, unique=True)
     description=models.CharField(max_length=100)
+    in_stock=models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
