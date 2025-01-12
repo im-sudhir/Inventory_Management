0f7fa2929cc6dd8e4bd5e0a8e63f09ab7b3bfb3e
im-sudhir <ssudhir794@gmail.com>
2025-01-12 20:36:48 +0530

    added filters for name and in_stock field

--- app/views.py
+++ app/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import InventoryItemSerializer
 from .models import InventoryItem
+from django_filters import rest_framework as filters
 # Create your views here.
+
+class NameFilter(filters.FilterSet):
+    name=filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    class Meta:
+        model=InventoryItem
+        fields=['name','in_stock']
 
 class ItemListCreateView(ListCreateAPIView):
     serializer_class=InventoryItemSerializer
     queryset=InventoryItem.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class=NameFilter
 
 class ItemDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=InventoryItemSerializer
