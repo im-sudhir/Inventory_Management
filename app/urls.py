81484f58d8ffec13d27bcb7eeddca9ce68a7c66e
im-sudhir <ssudhir794@gmail.com>
2025-01-14 23:19:08 +0530

    added jwt token authentication

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import ItemListCreateView, ItemDetailView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('items/', ItemListCreateView.as_view(), name="items"),
@@ -8,4 +13,10 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
+
+
+urlpatterns += [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .serializers import InventoryItemSerializer
 from .models import InventoryItem
 from django_filters import rest_framework as filters
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class NameFilter(filters.FilterSet):
@@ -17,7 +18,9 @@ class ItemListCreateView(ListCreateAPIView):
     queryset=InventoryItem.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class=NameFilter
+    permission_classes=[IsAuthenticated]
 
 class ItemDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class=InventoryItemSerializer
     queryset=InventoryItem.objects.all()
+    permission_classes=[IsAuthenticated]
